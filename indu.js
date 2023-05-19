00f49aa8c89894a633896c6b5d30c21e58ba05d5
JayeshChand <jayeshchand69@gmail.com>
2023-05-19 00:42:37 -1200

    code

---
+++ indu.js
@@ -0,0 +1,28 @@
+function add(){
+    let a=prompt("enter the no")
+    let b=prompt("enter trhe no")
+    a=parseInt(a)
+    b=parseInt(b)
+    document.writre(a+b,"<br>");
+}
+function sub(){
+    let a=prompt("enter the no")
+    let b=prompt("enter trhe no")
+    a=parseInt(a)
+    b=parseInt(b)
+    document.write(a-b,"<br>")
+}
+function multiply(){
+    let a=prompt("enter the no")
+    let b=prompt("enter trhe no")
+    a=parseInt(a)
+    b=parseInt(b)
+    document.write(a*b,"<br>")
+}
+function divide(){
+    let a=prompt("enter the no")
+    let b=prompt("enter trhe no")
+    document.write(a/b,"<br>")
+    a=parseInt(a)
+    b=parseInt(b)
+}
