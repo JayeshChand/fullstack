964591b8dd2c9ba612608e40c016b8569a88c3ae
JayeshChand <jayeshchand69@gmail.com>
2023-05-31 01:17:01 -1200

    countdown

---
+++ countdown.js
@@ -0,0 +1,13 @@
+function clock(){
+let endDate="31 december 2023 23:59"
+let end=new Date(endDate)
+let now=new Date()
+let inputs=document.querySelectorAll("input")
+let diff=(end-now)/1000
+console.log(diff);
+inputs[0].value=Math.floor(diff/3600/24);
+inputs[1].value=Math.floor(diff/3600)%24;
+inputs[2].value=Math.floor(diff/3600)%60;
+inputs[3].value=Math.floor(diff%60);
+}
+setInterval(()=>clock(),1000);
